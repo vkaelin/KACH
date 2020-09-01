439b2c036c38b8a076ad3fc1b1c8a5fee6ddba40
Valentin Kaelin <kalanehd@gmail.com>
2020-09-01 14:50:32 +0200

    chore: add nuxt boilerplate

---
+++ kach-conseils/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1>Kach-conseils</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel tenetur omnis architecto fuga! Fuga doloremque deserunt officia inventore possimus ea!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $prismic, params, error }) {
+    try {
+      const document = await $prismic.api.getByUID('homepage', 'homepage-fr')
+      if (document) {
+        return { document }
+      } else {
+        error({ statusCode: 404, message: 'Page not found' })
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
